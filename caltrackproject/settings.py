e1bb5ff6a83172cc20677ae256121262115312f1
isaacasher97 <isaac.asher97@gmail.com>
2023-09-23 20:31:53 -0400

    Created food model

--- caltrackproject/settings.py
+++ caltrackproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,14 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'foods.apps.FoodsConfig'
+    'foods.apps.FoodsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -75,10 +77,10 @@ WSGI_APPLICATION = 'caltrackproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    calories = models.IntegerField(default=0)
+    fat = models.IntegerField(default=0)
+    protein = models.IntegerField(default=0)
+    carbs = models.IntegerField(default=0)
+    sugar = models.IntegerField(default=0)
